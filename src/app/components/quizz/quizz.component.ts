f2b3ad28f0cf99b16e9884a22dece18216710e53
Luisk000 <luishenriqueangelini@gmail.com>
2025-01-29 18:28:48 -0300

    Início

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  standalone: false,
+  
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+
+}
